939293c1dddf9577a0efa8b2efe2988e833cf5e2
montohang <harmonsitohang17@gmail.com>
2021-01-06 13:00:54 +0700

    [Belajar Animated Container and Gesture Detector]

--- lib/pages/animated_container.dart
+++ lib/pages/animated_container.dart
@@ -6,12 +6,28 @@ class MyAnimatedContainer extends StatefulWidget {
 }
 
 class _MyAnimatedContainerState extends State<MyAnimatedContainer> {
+  Random random = Random();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Belajar Animated Containers"),
+        ),
+        body: Center(
+          child: GestureDetector(
+            onTap: () {
+              setState(() {});
+            },
+            child: AnimatedContainer(
+              color: Color.fromARGB(255, random.nextInt(256),
+                  random.nextInt(256), random.nextInt(256)),
+              duration: Duration(seconds: 1),
+              width: 50.0 + random.nextInt(100),
+              height: 50.0 + random.nextInt(100),
+            ),
+          ),
         ),
       ),
     );
